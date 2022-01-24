34e936c31c48f54a9e02729f16afe0ce1af7b9d1
shirish137 <55730469+shirish137@users.noreply.github.com>
2022-01-24 13:09:16 +0530

    Add files via upload
    Uploading project

---
+++ asset/js/project.js
@@ -0,0 +1,11 @@
+//Project Javascript File
+
+function register(){
+	
+	  //alert();
+	  var nobj=document.getElementById('name');
+	  alert(nobj.value);
+	  //var name=nobj.value;
+	  //window.location.href="../../python/store.py?n=name";
+	
+}
